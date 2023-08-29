04febd9e9713e85e295db91cde0e2c4eefe66020
Gavin Peng <gavin4@hawaii.edu>
2023-08-29 12:14:45 -1000

    rename file for todo backend

--- todo_react/authorization/views.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import viewsets, permissions
-from django.contrib.auth.models import User
-from authorization.serializers import UserSerializer
-
-
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-    permissions = [permissions.IsAuthenticated, permissions.IsAuthenticatedOrReadOnly]
