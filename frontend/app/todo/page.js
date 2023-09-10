a4dd3cda60ce1364e8cd73b9b8492d75264917b2
Gavin Peng <blackbullet54321@gmail.com>
2023-09-09 16:04:19 -1000

    create todos page

--- frontend/app/todo/page.js
+++ frontend/app/todo/page.js
@@ -1,18 +1,63 @@
 "use client"
 import FullContainer from "@/app/components/(mantine)/fullContainer";
-import { Button, Container, Group, Paper, TextInput } from "@mantine/core";
-import { IconCalendarEvent } from "@tabler/icons-react";
+import { Box, Button, Group, Paper, TextInput, Text, Container, Grid, SimpleGrid, Center } from "@mantine/core";
+import { useState } from "react";
+import { DateTimePicker } from "@mantine/dates";
+import { useForm } from "@mantine/form";
+import Todo from "@/app/todo/todo";
 
-export default function Todo() {
+export default function Todos() {
+    const [value, setValue] = useState(null);
+    const [isError, setError] = useState(false);
+    const form = useForm({
+        initialValues: {
+            date: '',
+        },
+
+        validate: {
+            date: (value) => {
+                const now = new Date();
+                if (now > value) {
+                    setError(true)
+                    return "This date is in the past"
+                }
+                setError(false)
+                return null
+            },
+        },
+        validateInputOnChange: true
+    });
     return (
-        <FullContainer h={600} mah={700}>
-            <Paper shadow="md" p="md">
-                <Group>
-                    <TextInput miw={500} placeholder="Your amazing task... " rightSection={<IconCalendarEvent/>}></TextInput>
-                    <Button>Create New Todo</Button>
-                </Group>
-                
-            </Paper>
-        </FullContainer>
+        <Box mt={30}>
+            <Container>
+                <SimpleGrid>
+                    <Paper shadow="md" p="md">
+                        <Group>
+                            <TextInput
+                                miw={500}
+                                placeholder="Your amazing task... "
+                            />
+                            <DateTimePicker
+                                clearable
+                                valueFormat="MMM DD YYYY hh:mm A"
+                                miw={150}
+                                mt={isError ? 17 : 0}
+                                defaultValue={new Date()}
+                                placeholder="Deadline"
+                                mx="auto"
+                                {...form.getInputProps('date')}
+                            />
+                            <Button>Create New Todo</Button>
+                        </Group>
+                    </Paper>
+                    <Paper shadow="md" p="md">
+                        <Center>
+                            <Text size="lg">No Todo</Text>
+                        </Center>
+                        <Todo/>
+                    </Paper>
+                </SimpleGrid>
+            </Container>
+        </Box>
     )
 }
